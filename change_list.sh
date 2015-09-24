85589a928f9aeb0841069f24d38cfec54b3c4cbd
darkdev57 <darkbouba57@gmail.com>
2015-09-24 21:23:30 +0200

    get file automatically and generate random ip

---
+++ change_list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/darkdev57/xbmcplaylist/master/playlist.m3u > playlist_new.m3u
+chmod 744 playlist_new.m3u
+myThing="85."$((RANDOM%6+1))"."$((RANDOM%254+1))"."$((RANDOM%254+1))
+sed -r "s/85\.([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/$myThing/g" playlist_new.m3u > playlist.m3u
+rm playlist_new.m3u
